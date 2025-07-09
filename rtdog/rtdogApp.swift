310480f6df6cda58472e8311916a202dc15bf181
daniel calderon <danieleduardocald@gmail.com>
2025-07-09 12:50:02 -0400

    Initial Commit

---
+++ rtdog/rtdogApp.swift
@@ -0,0 +1,17 @@
+//
+//  rtdogApp.swift
+//  rtdog
+//
+//  Created by Daniel Calderon on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct rtdogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ rtdogTests/rtdogTests.swift
@@ -0,0 +1,17 @@
+//
+//  rtdogTests.swift
+//  rtdogTests
+//
+//  Created by Daniel Calderon on 7/9/25.
+//
+
+import Testing
+@testable import rtdog
+
+struct rtdogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
